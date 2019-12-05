13005e648544632af0594ef26018166367b20bb9
julio <julio@octaltecnologia.com.br>
2019-12-05 15:58:23 -0300

    Criacao das funcoes para escrita do CSV e dicionario. Mudanca no codigo para ler toda playlist (100 em 100 tracks).

--- tests.py
+++ tests.py
@@ -6,11 +6,14 @@ from core import ExportPlaylist
 class TestExportPlaylist(unittest.TestCase):
 
     def setUp(self):
-        self.playlist_id = '4uVvklTsfJ9SBXJdxqgib6'
-        self.export = ExportPlaylist()
+        #self.playlist_id = '4uVvklTsfJ9SBXJdxqgib6'
+        self.playlist_id = '1yMK5IHUjTouRk74tiWmSa'
+        self.username = '12186735364'
+        self.export = ExportPlaylist(self.username)
 
     def test_export_spotify_playlist(self):
         teste = self.export.export_spotify_playlist(self.playlist_id)
-        self.assertEqual(teste, 2)
+        self.assertEqual(teste, 174)
 
 #spotify:playlist:0WAjP99fKlugTYbBkSWVOg
+#spotify:playlist:1yMK5IHUjTouRk74tiWmSa(MAN AT WORK)
